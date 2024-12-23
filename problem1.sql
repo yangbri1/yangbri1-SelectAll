524cb0d619144be637a75eb05ae66cc3dedf08e2
Brian Yang <yangbri1@msu.edu>
2024-12-23 23:43:32 +0000

    Practice using wildcard * to in Data Query Language (SQL) SELECT all records from DB table

---
+++ problem1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM song;
+-- Used wildcard * to SELECT/Show all records from 'song' database table (This way will show 'id' column as well NOT enumerated)
+-- Side Note: SQL keywords are case-insensitive, i.e. FROM == fRom == from etc.
